db85331075bb3af8f835ff8f13e1cde4bc2a7fe2
shnawaz844 <your_correct_email>
2026-02-05 16:49:27 +0530

    MyJobs.js Ui change

--- client/src/Pages/Candidate/Modal.js
+++ client/src/Pages/Candidate/Modal.js
@@ -202,7 +202,7 @@ const Modal = ({ getStatusColor, isOpen, onClose, app, getStatusName }) => {
                                                         {app.interview.meetingLink}
                                                     </a>
                                                 ) : (
-                                                    <div className="inline-flex items-center text-gray-400 font-medium break-all cursor-not-allowed">
+                                                    <div className="inline-flex items-center text-gray-400 font-medium break-all cursor-default">
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                                                         </svg>
